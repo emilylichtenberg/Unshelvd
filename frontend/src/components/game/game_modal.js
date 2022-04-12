0e59bb730bd6907b4755499897a9ad15e95d9324
emilylichtenberg <erlichtenberg@gmail.com>
2022-04-12 16:24:34 -0400

    Format buttons

--- frontend/src/components/game/game_modal.js
+++ frontend/src/components/game/game_modal.js
@@ -45,18 +45,29 @@
             <img src={modal.image} alt={modal.image} id="game-img--modal" />
 
             <div className='game-details'>
-              <h2 id="game-modal-num-players">Players: {modal.playerCount?.min} - {modal.playerCount?.max}</h2>
-              <h2 id="game-modal-category">Category: {modal.category?.map((cat, i) => {
-                            return (i === modal.category.length - 1) ? cat : cat + ", "
-              })}</h2>
-              <p id="game-modal-description">{modal.description}</p>
-              <h2 id="game-modal-setting">Setting: {modal.gameType}</h2>
+              <div>
+                <h2 id="game-modal-num-players">Players: </h2>
+                {modal.playerCount?.min} - {modal.playerCount?.max}
+              </div>
+              <div>
+                <h2 id="game-modal-category">Category:</h2>
+                {modal.category?.map((cat, i) => {
+                              return (i === modal.category.length - 1) ? cat : cat + ", "
+                })}
+              </div>
+              <div>
+                <p id="game-modal-description">{modal.description}</p>
+              </div>
+              <div>
+                <h2 id="game-modal-setting">Setting:</h2>
+                {modal.gameType}
+              </div>
             </div>
             
             <div>
                 { 
                 (!currentUser.games?.includes(modal._id)) ?
-                    <button id="button--add-game-to-library" onClick={() => this.addGameLibrary()}>Add Game to Library</button> : 
+                    <button className="button button--add-game-to-library" onClick={() => this.addGameLibrary()}>Add Game to Library</button> : 
                     <button className="button button--add-game-to-library" onClick={() => this.removeGameLibrary()}>Remove Game from Library</button>
                 }
             </div>
