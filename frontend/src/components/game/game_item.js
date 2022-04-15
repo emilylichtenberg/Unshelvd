d70a31c2f82a49dc2b47710253a2e046655c5534
emilylichtenberg <erlichtenberg@gmail.com>
2022-04-15 13:11:28 -0400

    add game title in browse

--- frontend/src/components/game/game_item.js
+++ frontend/src/components/game/game_item.js
@@ -10,7 +10,7 @@ class GameItem extends React.Component {
     return(
       <li className="library-index-item" onClick={() => openModal(game)}>
         <img src={game.image} alt={game.image} id="game-img--library"/>
-        <h1 id="game-title--library">{game.name}</h1>
+        <h1 id={game.image === "https://github.com/Bill-the-dev/Unshelvd_MERN/blob/main/z-images/game-piece-1.png?raw=true" ? "game-title--user-created" : "game-title--library"}>{game.name}</h1>
       </li>
     )
   }
